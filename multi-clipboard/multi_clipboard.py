45397f5c66f6109b8e6e6a0ad9cffe5ab48ebdd6
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-02-08 08:30:29 +0600

    Worked on Multi Clipboard

--- multi-clipboard/multi_clipboard.py
+++ multi-clipboard/multi_clipboard.py
@@ -8,3 +8,10 @@ SAVED_DATA = "clipboard.json"
 def save_data(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
+
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
