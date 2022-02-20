3bd50aafdb59fbeed03d36788853c7efa7674e4c
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-02-20 10:16:16 +0600

    Worked on Send Email

--- send-email/send_email.py
+++ send-email/send_email.py
@@ -4,3 +4,5 @@ from email.message import EmailMessage
 
 subject = "Test Email From Python"
 body = "This is a test email form Automation Python Project!"
+sender_email = "monwar.adeeb@gmail.com"
+receiver_email = "monwar.adeeb@gmail.com"
