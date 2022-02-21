5dd26bde1e9c5d15319a0220e35fd364d76be6bd
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-02-22 00:00:38 +0600

    Worked on Send Email

--- send-email/send_email.py
+++ send-email/send_email.py
@@ -7,3 +7,5 @@ body = "This is a test email form Automation Python Project!"
 sender_email = "monwar.adeeb@gmail.com"
 receiver_email = "monwar.adeeb@gmail.com"
 password = input("Enter a password: ")
+
+message = EmailMessage()
