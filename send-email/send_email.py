635182ce61be4fe9a92914977de12e614ce1e6b9
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-01 08:36:26 +0600

    Worked on Send Email

--- send-email/send_email.py
+++ send-email/send_email.py
@@ -27,3 +27,7 @@ message.add_alternative(html, subtype="html")
 context = ssl.create_default_context()
 
 print("Sending Automated Email!")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
