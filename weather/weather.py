22e70ba414a2cc29e37dbaf336188e0a9e7554b6
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-07 08:19:49 +0600

    Worked on Weather

--- weather/weather.py
+++ weather/weather.py
@@ -4,3 +4,4 @@ API_KEY = "f493235022f117443717201d55ff1d93"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
