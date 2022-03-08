206f4f33ffc7c2b6873f79b3865dfc8089582e71
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-08 08:33:12 +0600

    Worked on Weather

--- weather/weather.py
+++ weather/weather.py
@@ -5,3 +5,4 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
