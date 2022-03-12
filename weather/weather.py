88d3d36695703e75c65a2cb5ecf49879602e7bf0
Monwar Adeeb <monwar_adeeb@yahoo.com>
2022-03-12 10:54:47 +0600

    Worked on Weather

--- weather/weather.py
+++ weather/weather.py
@@ -11,3 +11,8 @@ if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
     temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("An error occurred.")
